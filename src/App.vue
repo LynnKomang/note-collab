b9a816aad3bfa22dab64e161904e1f5b3b909f16
LynnKomang <LynnKomang98@gmail.com>
2020-12-12 15:14:18 +0200

    Started working on the Task display.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container py-5">
+    <Task class="mx-auto col-4" :task="{title: 'Turn on the dishwasher', content: 'This is the description of the task', date: '1.1.2021', deadline: '1.1.2022'}" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Task from './components/Task.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Task
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card shadow rounded-lg">
+        <div class="card-body">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h3 class="card-title">{{ task.title }}</h3>
+                <p class="card-text text-muted">{{ task.date }}</p>
+            </div>
+            <p class="card-text">{{ task.content }}</p>
+            <p class="card-text text-danger">Deadline: {{ task.deadline }}</p>
+            <div class="text-end">
+                <button class="btn btn-primary">Edit</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        task: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
