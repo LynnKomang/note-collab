910cfea781fd406c5a0bc68b56ec2bf89d614c37
LynnKomang <LynnKomang98@gmail.com>
2020-12-13 14:15:57 +0200

    Changed structure of information retrieval.

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,59 @@
 </template>
 
 <script>
+import SockJS from "sockjs-client";
+import Stomp from "webstomp-client";
+
 import Task from './components/Task.vue'
 export default {
   name: 'App',
+  data: () => ({
+    returnedTask: null,
+    isConnected: false,
+  }),
   components: {
     Task
+  },
+  methods: {
+    getTasks() {
+      console.log("Getting Tasks from server.");
+
+      if (this.stompClient && this.stompClient.connected) {
+        this.stompClient.send("/app/tasks");
+      }
+    },
+    connect() {
+      this.socket = new SockJS("http://localhost:9000/gs-guide-websocket");
+      this.stompClient = Stomp.over(this.socket);
+
+      this.stompClient.connect({},
+      (frame) => {
+        this.isConnected = true;
+        console.log(frame);
+        this.stompClient.subscribe("/output/tasks", (tick) => {
+          console.log(tick);
+          this.returnedTask = JSON.parse(tick.body);
+        });
+        this.getTasks();
+      },
+      (error) => {
+        console.log(error);
+        this.isConnected = false;
+      });
+    },
+    disconnect() {
+      if (this.stompClient) {
+        this.stompClient.disconnect();
+      }
+
+      this.isConnected = false;
+    },
+    tickleConnection() {
+      this.isConnected ? this.disconnect() : this.connect();
+    },
+  },
+  created() {
+    this.connect();
   }
 }
 </script>
